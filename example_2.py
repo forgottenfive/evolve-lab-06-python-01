dd1093a8017e5cca5533a5fc5c88cd58364511c3
forgottenfive <forgottenfive@outlook.com>
2018-10-09 16:12:45 -0500

    Douglas's version of example.py
    Second commit

--- example_2.py
+++ example_2.py
@@ -1,13 +1,10 @@
 import requests
-def retrieve(my_url, my_exit_message = '\nExiting application.'):
+def retrieve(my_url):
     r = requests.get(my_url)
-    if r.raise_for_status().lower != "none":
-        print("Error", r.raise_for_status(), "thrown when attempting to access", my_url, my_exit_message)
-        exit()
-
+    r.raise_for_status()
     return r.text
 
-def file_write(my_txt, my_file):
+def file_write(my_txt, my_file, my_exit_message):
     try:
         f = open(my_file, "w")
         f.write(my_txt)
@@ -19,14 +16,16 @@ my_url = 'http://www.example.com'
 my_file = 'example_2.txt'
 my_exit_message = '\nExiting application.'
 
-my_txt = retrieve(my_url, my_exit_message)
+my_txt = retrieve(my_url)
 print("Successfully loaded", my_url)
 
 file_write(my_txt, my_file, my_exit_message)
-file_write(my_txt, my_file)
-print("Successfully wrote contents of", my_site, "to", my_file, my_exit_message)
+print("Successfully wrote contents of", my_url, "to", my_file)
 
-if input("Would you like to echo the text of", my_site, "to your terminal window? (Yn) "):
+if input("Would you like to echo the text of " + my_url + " to your terminal window? (Yn) ").lower() == 'y':
+    print("\nBody of", my_url, "\n\n")
     print(my_txt)
+    print("System message: EOF reached.")
+print(my_exit_message)
 exit()
 
